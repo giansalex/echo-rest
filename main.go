31f4d33f7468b0dd05d92c6e032b02d0683356da
Giancarlos Salas <giansalex@gmail.com>
2018-04-02 10:45:56 -0500

    add cors, add jwt

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"net/http"
+	"time"
 
+	jwt "github.com/dgrijalva/jwt-go"
 	"github.com/giansalex/echo-rest/model"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
@@ -14,12 +16,15 @@ func main() {
 	// Middleware
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
+	e.Use(middleware.CORS())
 
 	// Routes
 	e.GET("/", index).Name = "Index"
 	e.GET("/hello/:name", hello)
+	e.POST("/api/login", login)
 
 	api := e.Group("api/v1")
+	api.Use(middleware.JWT([]byte("secret")))
 	api.GET("/users", users)
 
 	// Start server
@@ -49,4 +54,34 @@ func users(c echo.Context) error {
 		},
 	}
 	return c.JSON(http.StatusOK, list)
+}
+
+func login(c echo.Context) (err error) {
+	auth := new(model.Auth)
+	if err = c.Bind(auth); err != nil {
+		return
+	}
+
+	if auth.Username == "admin" && auth.Password == "123456" {
+		// Create token
+		token := jwt.New(jwt.SigningMethodHS256)
+
+		// Set claims
+		claims := token.Claims.(jwt.MapClaims)
+		claims["name"] = "Giancarlos"
+		claims["admin"] = true
+		claims["exp"] = time.Now().Add(time.Hour * 72).Unix()
+
+		// Generate encoded token and send it as response.
+		t, err := token.SignedString([]byte("secret"))
+		if err != nil {
+			return err
+		}
+		return c.JSON(http.StatusOK, map[string]interface{}{
+			"token":  t,
+			"expire": claims["exp"],
+		})
+	}
+
+	return echo.ErrUnauthorized
 }
