2d2e23f736cc047047289c976b9be55c6ee0e04a
Giancarlos Salas <giansalex@gmail.com>
2018-04-02 10:20:59 -0500

    add api group

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 
+	"github.com/giansalex/echo-rest/model"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 )
@@ -18,6 +19,9 @@ func main() {
 	e.GET("/", index).Name = "Index"
 	e.GET("/hello/:name", hello)
 
+	api := e.Group("api/v1")
+	api.GET("/users", users)
+
 	// Start server
 	e.Logger.Fatal(e.Start(":1323"))
 }
@@ -31,4 +35,18 @@ func hello(c echo.Context) error {
 	name := c.Param("name")
 
 	return c.String(http.StatusOK, "Hello "+name)
+}
+
+func users(c echo.Context) error {
+	list := []*model.User{
+		&model.User{
+			Name:  "Jon",
+			Email: "jon@labstack.com",
+		},
+		&model.User{
+			Name:  "GianCarlos",
+			Email: "giansalex@gmail.com",
+		},
+	}
+	return c.JSON(http.StatusOK, list)
 }
